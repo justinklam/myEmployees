d84665b34c26c1f3f49c01f8a4015a77c1b67442
Justin Lam <justin.kh.lam@gmail.com>
2022-06-15 15:33:31 -0400

    add in top-bar logo

--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -5,7 +5,9 @@ const TopBar = () => {
   return (
     <div className="topnav">
       <div className="topnavWrapper">
-        <div className="topLeft">Left</div>
+        <div className="topLeft">
+          <span className="logo">myEmployees</span>
+        </div>
         <div className="topRight">Right</div>
       </div>
     </div>
