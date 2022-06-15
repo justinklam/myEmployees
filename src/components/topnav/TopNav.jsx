03ad2c0310c53fac41799cf002756c59d503df8e
Justin Lam <justin.kh.lam@gmail.com>
2022-06-15 16:04:29 -0400

    add icons for topnav and styling

--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./topNav.css";
 
+// Icons
+import SettingsIcon from "@mui/icons-material/Settings";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
+
 const TopBar = () => {
   return (
     <div className="topnav">
@@ -8,7 +12,19 @@ const TopBar = () => {
         <div className="topLeft">
           <span className="logo">myEmployees</span>
         </div>
-        <div className="topRight">Right</div>
+        <div className="topRight">
+          <div className="topnavIconContainer">
+            <SettingsIcon />
+          </div>
+          <div className="topnavIconContainer">
+            <LightbulbIcon />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/3184454/pexels-photo-3184454.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
       </div>
     </div>
   );
