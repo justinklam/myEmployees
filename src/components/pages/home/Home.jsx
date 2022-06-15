31e5b2f2ad13efef41536b7ae1fdde1e91714e48
Justin Lam <justin.kh.lam@gmail.com>
2022-06-15 19:20:04 -0400

    add in hover effects for topNav icons

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./home.css";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="homeWidget"></div>
+    </div>
+  );
 };
 
 export default Home;
