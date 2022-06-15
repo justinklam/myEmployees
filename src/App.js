5b26b04b03dd84db7daa3dc1bcfe37e3c5b3a204
Justin Lam <justin.kh.lam@gmail.com>
2022-06-15 15:27:38 -0400

    initialize elements for topNav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 // Components
+import TopNav from "./components/topnav/TopNav";
 import Employees from "./components/employee/Employees";
 
 const App = () => {
-  return <Employees />;
+  return (
+    <>
+      {/* <Employees /> */}
+      <TopNav />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/topnav/TopNav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./topNav.css";
+
+const TopBar = () => {
+  return (
+    <div className="topnav">
+      <div className="topnavWrapper">
+        <div className="topLeft">Left</div>
+        <div className="topRight">Right</div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
