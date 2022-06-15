5d62aef1bb578533a5a032622654d748ab549aec
Justin Lam <justin.kh.lam@gmail.com>
2022-06-15 13:10:42 -0400

    add in body-parser for put requests

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
+
+// Data
 const employees = require("./data/employees.json");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const corsOptions = {
   origin: "http://localhost:3000",
