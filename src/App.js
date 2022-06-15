63cf3ae76ffe8f25f79dfc610e8a8201571b1e0c
Justin Lam <justin.kh.lam@gmail.com>
2022-06-15 16:20:51 -0400

    add in icons for sideBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import "./app.css";
 
 // Components
 import TopNav from "./components/topnav/TopNav";
-import Employees from "./components/employee/Employees";
+import Sidebar from "./components/sidebar/Sidebar";
+// import Employees from "./components/employee/Employees";
 
 const App = () => {
   return (
     <>
       <TopNav />
+      <div className="container">
+        <Sidebar />
+      </div>
       {/* <Employees /> */}
     </>
   );
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./sidebar.css";
 
+// Icons
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import SettingsIcon from "@mui/icons-material/Settings";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper"></div>
+      <div className="sidebarMenu"></div>
+      <h3 className="sideBarTitle">Dashboard</h3>
+      <ul className="sidebarList">
+        <li className="sidebarListIcons">
+          <HomeIcon />
+        </li>
+        <li className="sidebarListIcons">
+          <PersonIcon />
+        </li>
+        <li className="sidebarListIcons">
+          <SettingsIcon />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
