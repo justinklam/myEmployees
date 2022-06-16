2e1881b0b0a23d87ee1ebe214ae52f6847f13afd
Justin Lam <justin.kh.lam@gmail.com>
2022-06-16 15:34:46 -0400

    initialize userList page

--- src/components/widgetL/WidgetL.jsx
+++ src/components/widgetL/WidgetL.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./widgetL.css";
 
 const WidgetL = () => {
-  return <div className="widgetLg">WidgetL</div>;
+  return (
+    <div className="widgetLg">
+      <h3 className="widgetLgTitle">Employee List</h3>
+    </div>
+  );
 };
 
 export default WidgetL;
