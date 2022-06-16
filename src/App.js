a8c51e0dc4044c0f9063e84ab642c15f8badf5b8
Justin Lam <justin.kh.lam@gmail.com>
2022-06-16 17:15:37 -0400

    add in react-router dom for page change. add in employeeList component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from "react";
 import "./app.css";
 
+// React Router Dom
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 // Components
 import TopNav from "./components/topnav/TopNav";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./components/pages/home/Home";
+import EmployeeList from "./components/pages/employeeList/EmployeeList";
 // import Employees from "./components/employee/Employees";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <TopNav />
       <div className="container">
         <Sidebar />
-        <Home />
-        {/* <div className="other-area">Other Pages</div> */}
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/employees">
+            <EmployeeList />
+          </Route>
+        </Switch>
       </div>
       {/* <Employees /> */}
-    </>
+    </Router>
   );
 };
 
---
+++ src/components/pages/employeeList/EmployeeList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./employeeList.css";
+
+const EmployeeList = () => {
+  return <div className="employeeList"></div>;
+};
+
+export default EmployeeList;
