ff1ad74162b6c746cf51f133a0737bbe5e814798
Justin Lam <justin.kh.lam@gmail.com>
2022-06-17 17:50:16 -0400

    add in form input boxes for new employee component

--- src/pages/newEmployee/NewEmployee.jsx
+++ src/pages/newEmployee/NewEmployee.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./newEmployee.css";
 
 const NewEmployee = () => {
-  return <div className="newEmployee">NewEmployee</div>;
+  return (
+    <div className="newEmployee">
+      <h1 className="newEmployeeTitle">New Employee</h1>
+      <form className="newEmployeeForm">
+        <div className="newEmployeeItem">
+          <label>First Name</label>
+          <input type="text" placeholder="First Name" />
+        </div>
+        <div className="newEmployeeItem">
+          <label>Last Name</label>
+          <input type="text" placeholder="Last Name" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default NewEmployee;
