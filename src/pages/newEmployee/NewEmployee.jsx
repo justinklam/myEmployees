42acb4aa95a98893a9752dfe072a237e37ee6285
Justin Lam <justin.kh.lam@gmail.com>
2022-06-17 17:53:06 -0400

    add in additional form input boxes

--- src/pages/newEmployee/NewEmployee.jsx
+++ src/pages/newEmployee/NewEmployee.jsx
@@ -14,6 +14,34 @@ const NewEmployee = () => {
           <label>Last Name</label>
           <input type="text" placeholder="Last Name" />
         </div>
+        <div className="newEmployeeItem">
+          <label>Profile</label>
+          <input type="text" placeholder="Profile" />
+        </div>
+        <div className="newEmployeeItem">
+          <label>Email</label>
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="newEmployeeItem">
+          <label>Code</label>
+          <input type="text" placeholder="Code" />
+        </div>
+        <div className="newEmployeeItem">
+          <label>Profession</label>
+          <input type="text" placeholder="Profession" />
+        </div>
+        <div className="newEmployeeItem">
+          <label>City</label>
+          <input type="text" placeholder="City" />
+        </div>
+        <div className="newEmployeeItem">
+          <label>Branch</label>
+          <input type="text" placeholder="Branch" />
+        </div>
+        <div className="newEmployeeItem">
+          <label>Assigned</label>
+          <input type="text" placeholder="Assigned" />
+        </div>
       </form>
     </div>
   );
