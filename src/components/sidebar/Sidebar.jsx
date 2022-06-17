49b51717cfbe110a2bd7efc3d561dccebc7b4fc3
Justin Lam <justin.kh.lam@gmail.com>
2022-06-17 18:23:39 -0400

    add in router-dom link to add links in Sidebar. add in link class and styling

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./sidebar.css";
+import { Link } from "react-router-dom";
 
 // Icons
 import HomeIcon from "@mui/icons-material/Home";
 import PersonIcon from "@mui/icons-material/Person";
 import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
-import PersonRemoveAlt1Icon from "@mui/icons-material/PersonRemoveAlt1";
+// import PersonRemoveAlt1Icon from "@mui/icons-material/PersonRemoveAlt1";
 import SettingsIcon from "@mui/icons-material/Settings";
 
 const Sidebar = () => {
@@ -15,34 +16,46 @@ const Sidebar = () => {
       <div className="sidebarMenu"></div>
       <h3 className="sidebarTitle">Dashboard</h3>
       <ul className="sidebarList">
-        <li className="sidebarListIcons active">
-          <HomeIcon className="sidebarIcon" />
-          Home
-        </li>
+        <Link to="/" className="link">
+          <li className="sidebarListIcons active">
+            <HomeIcon className="sidebarIcon" />
+            Home
+          </li>
+        </Link>
       </ul>
       <div className="sidebarMenu"></div>
       <h3 className="sidebarTitle">Personnel</h3>
       <ul className="sidebarList">
-        <li className="sidebarListIcons">
-          <PersonIcon className="sidebarIcon" />
-          Search
-        </li>
-        <li className="sidebarListIcons">
-          <PersonAddAlt1Icon className="sidebarIcon" />
-          Add
-        </li>
-        <li className="sidebarListIcons">
-          <PersonRemoveAlt1Icon className="sidebarIcon" />
-          Remove
-        </li>
+        <Link to="/employees" className="link">
+          <li className="sidebarListIcons">
+            <PersonIcon className="sidebarIcon" />
+            Search
+          </li>
+        </Link>
+
+        <Link to="/newEmployee" className="link">
+          <li className="sidebarListIcons">
+            <PersonAddAlt1Icon className="sidebarIcon" />
+            Add
+          </li>
+        </Link>
+
+        {/* <Link to="/removeEmployee" className="sideBarLink">
+          <li className="sidebarListIcons">
+            <PersonRemoveAlt1Icon className="sidebarIcon" />
+            Remove
+          </li>
+        </Link> */}
       </ul>
       <div className="sidebarMenu"></div>
       <h3 className="sidebarTitle">Admin</h3>
       <ul className="sidebarList">
-        <li className="sidebarListIcons">
-          <SettingsIcon className="sidebarIcon" />
-          Settings
-        </li>
+        <Link to="/settings" className="link">
+          <li className="sidebarListIcons">
+            <SettingsIcon className="sidebarIcon" />
+            Settings
+          </li>
+        </Link>
       </ul>
     </div>
   );
