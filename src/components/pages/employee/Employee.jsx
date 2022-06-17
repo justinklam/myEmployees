610d5e079ada6e72cdb24156788cf35aee0687f8
Justin Lam <justin.kh.lam@gmail.com>
2022-06-17 00:06:36 -0400

    add in employee component elements for title

--- src/components/pages/employee/Employee.jsx
+++ src/components/pages/employee/Employee.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./employee.css";
 
 const Employee = () => {
-  return <div className="employee">Employee</div>;
+  return (
+    <div className="employee">
+      <div className="employeeTitleContainer">
+        <h1 className="employeeTitle">Edit Employee</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Employee;
