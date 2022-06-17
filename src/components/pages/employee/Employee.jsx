2910e65dcc14e6a5b0b0f9a95fb8c7d0f23eb191
Justin Lam <justin.kh.lam@gmail.com>
2022-06-17 11:21:51 -0400

    add in employee component elements for title and info section. add in base styling

--- src/components/pages/employee/Employee.jsx
+++ src/components/pages/employee/Employee.jsx
@@ -9,7 +9,22 @@ const Employee = () => {
         <button className="employeeAddButton">Create</button>
       </div>
       <div className="employeeContainer">
-        <div className="employeeShow"></div>
+        <div className="employeeShow">
+          <div className="employeeShowTop">
+            <img
+              src="https://images.pexels.com/photos/5468093/pexels-photo-5468093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className="employeeShowImg"
+            />
+            <div className="employeeShowTopTitle">
+              <span className="employeeShowUsername">Kyle Lowry</span>
+              <span className="employeeShowUserProfession">
+                Drywall Installer
+              </span>
+            </div>
+          </div>
+          <div className="employeeShowBottom"></div>
+        </div>
         <div className="employeeUpdate"></div>
       </div>
     </div>
