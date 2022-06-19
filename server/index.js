d12fd5eda8b07694fd37991991b611573e180348
Justin Lam <justin.kh.lam@gmail.com>
2022-06-19 17:48:02 -0400

    shift data into index.js add logic for app.delete route and app.post

--- server/index.js
+++ server/index.js
@@ -1,9 +1,129 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const fs = require("fs");
 
 // Data
-const employees = require("./data/employees.json");
+// const employees = require("./data/employees.json");
+const employees = [
+  {
+    id: 1,
+    lastName: "Lowry",
+    firstName: "Kyle",
+    profile:
+      "https://images.pexels.com/photos/5468093/pexels-photo-5468093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "user1@mail.com",
+    code: "F100",
+    profession: "Drywall Installer",
+    city: "Toronto",
+    branch: "Abacus",
+    assigned: "True",
+  },
+  {
+    id: 2,
+    lastName: "DeRozan",
+    firstName: "DeMar",
+    profile:
+      "https://images.pexels.com/photos/1752757/pexels-photo-1752757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "user1@mail.com",
+    code: "F102",
+    profession: "Drywall Installer",
+    city: "Brampton",
+    branch: "Pillsworth",
+    assigned: "True",
+  },
+  {
+    id: 3,
+    lastName: "Van Vleet",
+    firstName: "Fred",
+    profile:
+      "https://images.pexels.com/photos/1752757/pexels-photo-1752757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "user1@mail.com",
+    code: "F103",
+    profession: "Drywall Installer",
+    city: "Bolton",
+    branch: "Abacus",
+    assigned: "True",
+  },
+  {
+    id: 4,
+    lastName: "Valanciunas",
+    firstName: "Jonas",
+    profile:
+      "https://images.pexels.com/photos/1752757/pexels-photo-1752757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "user1@mail.com",
+    code: "F104",
+    profession: "Drywall Installer",
+    city: "Bolton",
+    branch: "Abacus",
+    assigned: "True",
+  },
+  {
+    id: 5,
+    lastName: "Bosh",
+    firstName: "Chris",
+    profile:
+      "https://images.pexels.com/photos/1752757/pexels-photo-1752757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "user1@mail.com",
+    code: "F105",
+    profession: "Drywall Installer",
+    city: "Brampton",
+    branch: "Abacus",
+    assigned: "True",
+  },
+  {
+    id: 6,
+    lastName: "Camby",
+    firstName: "Marcus",
+    profile:
+      "https://images.pexels.com/photos/1752757/pexels-photo-1752757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "user1@mail.com",
+    code: "F106",
+    profession: "Drywall Installer",
+    city: "Brampton",
+    branch: "Abacus",
+    assigned: "True",
+  },
+  {
+    id: 7,
+    lastName: "Carter",
+    firstName: "Vince",
+    profile:
+      "https://images.pexels.com/photos/1752757/pexels-photo-1752757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "user1@mail.com",
+    code: "F107",
+    profession: "Drywall Installer",
+    city: "Toronto",
+    branch: "Abacus",
+    assigned: "True",
+  },
+  {
+    id: 8,
+    lastName: "Frances",
+    firstName: "Rossini",
+    profile:
+      "https://images.pexels.com/photos/1752757/pexels-photo-1752757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "user1@mail.com",
+    code: "F108",
+    profession: "Drywall Installer",
+    city: "Toronto",
+    branch: "Abacus",
+    assigned: "True",
+  },
+  {
+    id: 9,
+    lastName: "Roxie",
+    firstName: "Harvey",
+    profile:
+      "https://images.pexels.com/photos/1752757/pexels-photo-1752757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "user1@mail.com",
+    code: "F109",
+    profession: "Drywall Installer",
+    city: "Toronto",
+    branch: "Abacus",
+    assigned: "True",
+  },
+];
 
 const app = express();
 
@@ -22,7 +142,7 @@ const corsOptions = {
 };
 
 // Retrieve Employees
-app.get("/api/employees", cors(corsOptions), (req, res, next) => {
+app.get("/api/employees", cors(corsOptions), (req, res) => {
   console.log("get - /api/employees");
   res.setHeader("Content-Type", "application/json");
   res.status(200);
@@ -30,25 +150,37 @@ app.get("/api/employees", cors(corsOptions), (req, res, next) => {
 });
 
 // Create Employee
-app.post("/api/createEmployee", cors(corsOptions), (req, res, next) => {
+app.post("/api/employees/create", cors(corsOptions), (req, res) => {
   console.log("post-create - /api/employees");
-  res.setHeader("Content-Type", "application/json");
-  res.status(200);
-  res.send(JSON.stringify(employees, null, 2));
+  employees.push(req.body);
+  console.log("Success");
+  res.json("Employee Created");
+  console.log("after post", employees);
+
+  // fs.writeFile(
+  //   employees,
+  //   JSON.stringify(req.body, null, 2),
+  //   function writeJSON(err) {
+  //     if (err) return console.log("Post-Error", err);
+  //     console.log(JSON.stringify(req.body));
+  //     console.log("Adding ", req.body);
+  //   }
+  // );
 });
 
 // Update Employee
-app.put("/api/updateEmployee", cors(corsOptions), (req, res, next) => {
+app.put("/api/employees/update", cors(corsOptions), (req, res) => {
   console.log("put-update - /api/employees");
-  res.setHeader("Content-Type", "application/json");
-  res.status(200);
-  res.send(JSON.stringify(employees, null, 2));
 });
 
 // Delete Employee
-app.delete("/api/deleteEmployee", cors(corsOptions), (req, res, next) => {
+app.delete("/api/employees/delete/:id", cors(corsOptions), (req, res) => {
   console.log("delete - /api/employees");
-  res.setHeader("Content-Type", "application/json");
+  const index = employees.indexOf(
+    employees.find((value) => value.id === req.params.id.toString())
+  );
+  employees.splice(index, 1);
+  console.log("Employee deleted successfully");
   res.status(200);
   res.send(JSON.stringify(employees, null, 2));
 });
