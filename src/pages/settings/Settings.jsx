178426e9f08d584c164d6f7631b808ce1b2f34ad
Justin Lam <justin.kh.lam@gmail.com>
2022-06-20 01:24:29 -0400

    add in base elements and styling

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./settings.css";
 
 const Settings = () => {
-  return <div className="settings">Settings</div>;
+  return (
+    <div className="settings">
+      <div className="settingsTitleContainer">
+        <h1 className="settingsTitle">Settings</h1>
+      </div>
+      <div className="settingsContainer">
+        <div className="settingsShow"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
