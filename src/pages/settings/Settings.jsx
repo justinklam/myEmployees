7b059f1dc58b26032cd8330df1ee7911e0a1a487
Justin Lam <justin.kh.lam@gmail.com>
2022-06-24 00:58:00 -0400

    populate with placeholder data/styling

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -8,7 +8,63 @@ const Settings = () => {
         <h1 className="settingsTitle">Settings</h1>
       </div>
       <div className="settingsContainer">
-        <div className="settingsShow"></div>
+        <div className="settingsShow">
+          <div className="settingShowItem">
+            <label>Settings 1</label>
+            <select
+              name="settingsChoice"
+              id="settingsChoice"
+              className="settingsItem"
+            >
+              <option value="true">True</option>
+              <option value="false">False</option>
+            </select>
+          </div>
+          <div className="settingShowItem">
+            <label>Settings 2</label>
+            <select
+              name="settingsChoice"
+              id="settingsChoice"
+              className="settingsItem"
+            >
+              <option value="true">True</option>
+              <option value="false">False</option>
+            </select>
+          </div>
+          <div className="settingShowItem">
+            <label>Settings 3</label>
+            <select
+              name="settingsChoice"
+              id="settingsChoice"
+              className="settingsItem"
+            >
+              <option value="true">True</option>
+              <option value="false">False</option>
+            </select>
+          </div>
+          <div className="settingShowItem">
+            <label>Settings 4</label>
+            <select
+              name="settingsChoice"
+              id="settingsChoice"
+              className="settingsItem"
+            >
+              <option value="true">True</option>
+              <option value="false">False</option>
+            </select>
+          </div>
+          <div className="settingShowItem">
+            <label>Settings 5</label>
+            <select
+              name="settingsChoice"
+              id="settingsChoice"
+              className="settingsItem"
+            >
+              <option value="true">True</option>
+              <option value="false">False</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   );
