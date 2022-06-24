565e985a962e6202123f821491b1e8350ebd7789
Justin Lam <justin.kh.lam@gmail.com>
2022-06-23 23:32:11 -0400

    import in MUI icons

--- src/components/widgetL/WidgetL.jsx
+++ src/components/widgetL/WidgetL.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import "./widgetL.css";
 
+// MUI
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import CodeIcon from "@mui/icons-material/Code";
+import WorkIcon from "@mui/icons-material/Work";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import ParkIcon from "@mui/icons-material/Park";
+import EmailIcon from "@mui/icons-material/Email";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+
 const WidgetL = () => {
   return (
     <div className="widgetLg">
